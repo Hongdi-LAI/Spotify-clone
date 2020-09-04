3cad7a1d01f7a9d1006dcba6f39b415ef7222928
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-04 16:03:17 +0200

    Spotify Login Page Setup

---
+++ spotify-clone/src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className = "login">
+            {/* Spotify logo and login with spotify button */}
+            <img 
+            src = "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
+            alt="" 
+            />
+
+            <a>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
